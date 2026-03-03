943e3f8cc14e68b378b33d179a8c6ecb5aed293d
Davi Curvelo <davi.curvelo03@gmail.com>
2026-03-03 13:19:15 -0300

    erro de registrar produto 2

--- model.py
+++ model.py
@@ -11,8 +11,8 @@ class produto(db.Model):
     __tablename__ = 'Produtos'
 
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String, nullable=False)
+    nome = db.Column(db.String(50), nullable=False)
     descricao = db.Column(db.Text, nullable=True)
     preco = db.Column(db.Float, nullable=True)
     quantidade = db.Column(db.Integer, nullable=True)
-    imagem = db.Column(db.String, nullable=True)
+    imagem = db.Column(db.String(500), nullable=True)

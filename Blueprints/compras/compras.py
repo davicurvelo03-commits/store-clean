3c1e3bb482faf07a2921e16118aa34b9f365ea04
Davi Curvelo <davi.curvelo03@gmail.com>
2026-03-08 14:32:28 -0300

    erro produtos

--- Blueprints/compras/compras.py
+++ Blueprints/compras/compras.py
@@ -1,7 +1,5 @@
 from flask import blueprints
 from flask import render_template , request
-from model import User
-from db import db
 from model import produto
 
 compras_bp = blueprints.Blueprint('compras', __name__, template_folder='templates.compra')

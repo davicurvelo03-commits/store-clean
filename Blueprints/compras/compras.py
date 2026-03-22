4a8bd37ba7cfd3cdd40d6ae3e5ba39de469904b5
Davi Curvelo <davi.curvelo03@gmail.com>
2026-03-21 22:02:11 -0300

    compras

--- Blueprints/compras/compras.py
+++ Blueprints/compras/compras.py
@@ -4,6 +4,7 @@ from model import produto
 
 compras_bp = blueprints.Blueprint('compras', __name__, template_folder='templates.compra')
 pagamento_bp = blueprints.Blueprint('pagamento', __name__, template_folder='templates.compra')
+
 @compras_bp.route('/compras',methods=['GET','POST'])
 def compras():
     produtos = produto.query.all()
--- main.py
+++ main.py
@@ -21,14 +21,22 @@ cloudinary.config(
     api_secret=os.getenv("CLOUDINARY_API_SECRET")
 )
 print("CLOUD:", os.getenv("CLOUDINARY_CLOUD_NAME"))
-
 database_url = os.getenv("DATABASE_URL")
 
 if database_url and database_url.strip() != "":
     if database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
+
     app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+
+    if "postgresql" in database_url:
+        app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+            "connect_args": {
+                "sslmode": "require"
+            }
+        }
 else:
+
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///trabalho.db"
 
 db.init_app(app)
